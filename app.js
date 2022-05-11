5ddee541c27d36d34493138c96e905da390245dc
Rodan Ramdam <rodanramdam0818@gmail.com>
2022-05-11 15:48:39 +0545

    add and remove implemented (no use of any arrow function)

---
+++ app.js
@@ -0,0 +1,44 @@
+const yargs = require("yargs");
+const notes = require("./notes");
+
+//Adding version
+yargs.version("1.0.0");
+//Adding commands add, remove, list, read
+//For adding note
+yargs.command({
+  command: "add",
+  describe: "Add a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note Body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    // console.log("Title: " + argv.title + "\nBody: " + argv.body);
+    notes.addNotes(argv.title, argv.body);
+  },
+});
+//For removing note
+yargs.command({
+  command: "remove",
+  describe: "Remove a note using title name provided",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    // console.log(argv.title);
+    notes.removeNotes(argv.title);
+  },
+});
+yargs.parse();
---
+++ notes.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+const chalk = require("chalk");
+const getNotes = function () {
+  //Return Notes
+};
+const addNotes = function (title, body) {
+  //Adds a note
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter(function (note) {
+    return note.title === title;
+  });
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+    saveNotes(notes);
+    console.log(chalk.bgGreen.bold("New Note Added!"));
+  } else {
+    console.log(chalk.red.inverse("Note title taken"));
+  }
+};
+const removeNotes = function (title) {
+  const notes = loadNotes();
+  const newNotes = notes.filter(function (note) {
+    return note.title !== title;
+  });
+  if (newNotes.length === notes.length) {
+    console.log(chalk.red.inverse.bold("No any note with the title: " + title));
+  } else {
+    saveNotes(newNotes);
+    console.log(
+      chalk.bgGreen.bold("Successfully deleted note with the title: " + title)
+    );
+  }
+};
+const loadNotes = function () {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+const saveNotes = function (notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+module.exports = {
+  addNotes: addNotes,
+  removeNotes: removeNotes,
+};
